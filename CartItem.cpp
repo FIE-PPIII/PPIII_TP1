0aa1dd19ca61c10c5a640814ca5f25d1a77068bf
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-09-16 20:24:21 -0300

    ADD - CartItem method definitions

--- CartItem.cpp
+++ CartItem.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "CartItem.h"
+
+#include <utility>
+
+using namespace std;
+
+CartItem::CartItem(string detail, unsigned int quantity, unsigned int price) {
+    this->detail = std::move(detail);
+    this->quantity = quantity;
+    this->price = price;
+}
+
+CartItem::CartItem(string detail, unsigned int quantity, float price) {
+    this->detail = std::move(detail);
+    this->quantity = quantity;
+    this->price = (unsigned int) (100 * price);
+}
+
+CartItem::~CartItem() = default;
+
+unsigned long int CartItem::getSubtotal() {
+    return this->price * this->quantity;
+}
--- CartItem.h
+++ CartItem.h
@@ -17,11 +17,24 @@ private:
 public:
     CartItem(string detail, unsigned int quantity, unsigned int price);
     CartItem(string detail, unsigned int quantity, float price);
+    ~CartItem();
     string getDetail();
     unsigned int getQuantity();
     unsigned int getPrice();
     unsigned long int getSubtotal();
 };
+
+inline string CartItem::getDetail() {
+    return this->detail;
+}
+
+inline unsigned int CartItem::getQuantity() {
+    return this->quantity;
+}
+
+inline unsigned int CartItem::getPrice() {
+    return this->price;
+}
 
 
 #endif //BASE_CPP_CARTITEM_H
